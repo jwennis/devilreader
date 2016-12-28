b0408ababc2bfd933045a283ca4e68b9e92a68a4
Joe <j.w.ennis@gmail.com>
2016-12-27 23:06:05 -0500

    feat: Add Firebase to project

--- app/src/main/java/com/example/dreader/devilreader/NewsFragment.java
+++ app/src/main/java/com/example/dreader/devilreader/NewsFragment.java
@@ -2,9 +2,16 @@ package com.example.dreader.devilreader;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class NewsFragment extends Fragment {
@@ -19,7 +26,28 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        getActivity().setTitle("");
+        getActivity().setTitle(R.string.drawer_news);
+
+
+        FirebaseDatabase instance = FirebaseDatabase.getInstance();
+        DatabaseReference reference = instance.getReference("Story");
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot data) {
+
+                for(DataSnapshot snapshot : data.getChildren()) {
+
+                    Log.v("DREADER", "STORY " + snapshot.child("title").getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         return inflater.inflate(R.layout.fragment_news, container, false);
     }
