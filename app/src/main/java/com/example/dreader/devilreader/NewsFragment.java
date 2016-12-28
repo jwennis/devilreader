18680c53e2cde89c260c3db1e7777cf01a87d4e8
Joe <j.w.ennis@gmail.com>
2016-12-28 13:39:37 -0500

    feat: Add Story filter criteria
    Filters the RecyclerView to show all, unread or saved stories.
    - Add filter option in settings menu
    - Create popup dialog to choose filter
    - Restart loader upon filter criteria change
    - Persist state upon configuration change

--- app/src/main/java/com/example/dreader/devilreader/NewsFragment.java
+++ app/src/main/java/com/example/dreader/devilreader/NewsFragment.java
@@ -1,35 +1,49 @@
 package com.example.dreader.devilreader;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Dialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.dreader.devilreader.data.StoryContract.StoryEntry;
-import com.example.dreader.devilreader.firebase.FirebaseUtil;
-import com.example.dreader.devilreader.model.Story;
-import com.example.dreader.devilreader.sync.StorySyncAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import com.example.dreader.devilreader.data.StoryContract.StoryEntry;
+import com.example.dreader.devilreader.model.Story;
+import com.example.dreader.devilreader.sync.StorySyncAdapter;
+
+
 public class NewsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String PARAM_FILTER = "PARAM_FILTER";
 
     private static final int LOADER_ID = 0;
 
     private StoryAdapter mAdapter;
+    private Filter mFilter;
+
     private ViewGroup layout_root;
 
     @BindView(R.id.news_recycler)
@@ -45,6 +59,15 @@ public class NewsFragment extends Fragment implements LoaderManager.LoaderCallba
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        if (savedInstanceState != null) {
+
+            mFilter = (Filter) savedInstanceState.getSerializable(PARAM_FILTER);
+
+        } else {
+
+            mFilter = Filter.UNREAD;
+        }
+
         layout_root = (ViewGroup) inflater.inflate(R.layout.fragment_news, container, false);
 
         bindNews();
@@ -52,17 +75,6 @@ public class NewsFragment extends Fragment implements LoaderManager.LoaderCallba
         return layout_root;
     }
 
-    private void bindNews() {
-
-        ButterKnife.bind(this, layout_root);
-
-        recycler.setLayoutManager(new LinearLayoutManager(getContext()));
-        recycler.setItemAnimator(new DefaultItemAnimator());
-
-        getActivity().setTitle(R.string.drawer_news);
-
-        StorySyncAdapter.syncImmediately(getContext());
-    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -75,13 +87,75 @@ public class NewsFragment extends Fragment implements LoaderManager.LoaderCallba
 
 
     @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        outState.putSerializable(PARAM_FILTER, mFilter);
+
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.news, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        FragmentManager manager = getActivity().getSupportFragmentManager();
+        DialogFragment dialog;
+
+        switch(item.getItemId()) {
+
+            case R.id.action_filter: {
+
+                dialog = new FilterDialogFragment();
+
+                Bundle args = new Bundle();
+                args.putSerializable(PARAM_FILTER, mFilter);
+
+                dialog.setArguments(args);
+                dialog.setTargetFragment(this, 0);
+                dialog.show(manager, FilterDialogFragment.FRGAMENT_TAG);
+
+                return true;
+            }
+
+            default: {
+
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
+
+
+    @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 
         if(id == LOADER_ID) {
 
+            StringBuilder selectionBuilder = new StringBuilder();
+            List<String> argsList = new ArrayList<>();
+
+            if(mFilter == Filter.SAVED) {
+
+                selectionBuilder.append(StoryEntry.COL_IS_SAVED + " > ?");
+                argsList.add("0");
+
+            } else if (mFilter == Filter.UNREAD) {
+
+                selectionBuilder.append(StoryEntry.COL_IS_READ + " = ?");
+                argsList.add("0");
+            }
+
             String[] projection = new String[] { };
-            String selection = "";
-            String[] selectionArgs = new String[] { };
+            String selection = selectionBuilder.toString();
+            String[] selectionArgs = argsList.toArray(new String[argsList.size()]);
             String sortOrder = String.format("%1$s DESC", StoryEntry.COL_PUBDATE);
 
             return new CursorLoader(getContext(), StoryEntry.CONTENT_URI,
@@ -117,8 +191,22 @@ public class NewsFragment extends Fragment implements LoaderManager.LoaderCallba
 
         if(loader.getId() == LOADER_ID) {
 
-            // TODO: swap cursor
+            mAdapter.swapCursor(null);
         }
+    }
+
+
+    private void bindNews() {
+
+        ButterKnife.bind(this, layout_root);
+
+        getActivity().setTitle(R.string.drawer_news);
+        setHasOptionsMenu(true);
+
+        recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        recycler.setItemAnimator(new DefaultItemAnimator());
+
+        StorySyncAdapter.syncImmediately(getContext());
     }
 
 
@@ -158,5 +246,69 @@ public class NewsFragment extends Fragment implements LoaderManager.LoaderCallba
 
         ItemTouchHelper swipeHelper = new ItemTouchHelper(swipeCallback);
         swipeHelper.attachToRecyclerView(recycler);
+    }
+
+
+    /**
+     * Used to filter which stories are shown in the RecyclerView
+     */
+    private enum Filter {
+
+        ALL(0), SAVED(1), UNREAD(2);
+
+        private final int value;
+        private Filter(int v) { this.value = v; }
+        public int getValue() { return value; }
+    }
+
+
+    /**
+     * Sets the filter criteria and restarts the loader
+     *
+     * @param filter the chosen filter criteria
+     */
+    private void applyFilter(Filter filter) {
+
+        if(mFilter != filter) {
+
+            mFilter = filter;
+
+            getActivity().getSupportLoaderManager()
+                    .restartLoader(LOADER_ID, null, this);
+        }
+    }
+
+
+    /**
+     * Creates a popup dialog to choose the filter criteria
+     */
+    public static class FilterDialogFragment extends DialogFragment {
+
+        public static final String FRGAMENT_TAG = "FilterDialogFragment";
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            Filter activeFilter = (Filter) getArguments().getSerializable(PARAM_FILTER);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.action_filter);
+
+            builder.setSingleChoiceItems(R.array.settings_news_filters, activeFilter.getValue(),
+                    new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int index) {
+
+                            ((NewsFragment) getTargetFragment())
+                                    .applyFilter(Filter.values()[index]);
+
+                            dismiss();
+                        }
+                    });
+
+            return builder.create();
+        }
     }
 }
