bb4dda3be9094577d75bd5d6ad948b5c954652b0
Joe <j.w.ennis@gmail.com>
2016-12-28 14:56:24 -0500

    feat: Set up back button and slide in/out transition

--- app/src/main/java/com/example/dreader/devilreader/StoryActivity.java
+++ app/src/main/java/com/example/dreader/devilreader/StoryActivity.java
@@ -2,6 +2,7 @@ package com.example.dreader.devilreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.dreader.devilreader.model.Story;
 
@@ -17,6 +18,10 @@ public class StoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_story);
+
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if(savedInstanceState != null) {
 
@@ -35,5 +40,32 @@ public class StoryActivity extends AppCompatActivity {
         outState.putParcelable(Story.PARAM_STORY_PARCEL, mStory);
 
         super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+
+            case android.R.id.home: {
+
+                onBackPressed();
+            }
+
+            default: {
+
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        super.onBackPressed();
+
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
     }
 }
