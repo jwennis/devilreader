86d5fbc1072832d0464263c51d92b41b7c4c169b
Joe <j.w.ennis@gmail.com>
2017-01-02 12:24:55 -0500

    feat: Implement Parcelable on Game model

--- app/src/main/java/com/example/dreader/devilreader/model/Game.java
+++ app/src/main/java/com/example/dreader/devilreader/model/Game.java
@@ -1,38 +1,96 @@
 package com.example.dreader.devilreader.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 
 import com.example.dreader.devilreader.Util;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class Game {
+public class Game implements Parcelable {
+
+    private long nhl_id;
+    private long datestring;
+
+    private String opponent;
+    private String location;
+
+    private String status;
+    private String puckdrop;
+    private String networks;
 
     private String article;
-    private long datestring;
+    private String tagline;
+
+    private String recapImage;
+    private String recapVideo;
 
     private List<String> goalsAway;
     private List<String> goalsHome;
 
-    private String location; // "H" or "A"
-    private long nhl_id;
-
-    private String opponent;
-    private String recapImage;
-    private String recapVideo;
-
     private List<String> statsAway;
     private List<String> statsHome;
 
-    private String status;
-    private String tagline;
-
-    private String puckdrop;
-    private String networks;
 
     public Game() {
 
+    }
+
+
+    public Game(Parcel in) {
+
+        nhl_id = in.readLong();
+        datestring = in.readLong();
+
+        opponent = in.readString();
+        location = in.readString();
+
+        status = in.readString();
+
+        if(status.equals("P")) {
+
+            puckdrop = in.readString();
+            networks = in.readString();
+
+        } else {
+
+            article = in.readString();
+            tagline = in.readString();
+
+            recapImage = in.readString();
+            recapVideo = in.readString();
+
+            goalsAway = new ArrayList<>();
+            goalsHome = new ArrayList<>();
+
+            int numFrames = status.equals("F") ? 4 : 5;
+
+            for(int i = 0; i < numFrames; i ++) {
+
+                goalsAway.add(in.readString());
+            }
+
+            for(int i = 0; i < numFrames; i ++) {
+
+                goalsHome.add(in.readString());
+            }
+
+            statsAway = new ArrayList<>();
+            statsHome = new ArrayList<>();
+
+            for(int i = 0; i < 7; i ++) {
+
+                statsAway.add(in.readString());
+            }
+
+            for(int i = 0; i < 7; i ++) {
+
+                statsHome.add(in.readString());
+            }
+        }
     }
 
 
@@ -129,9 +187,8 @@ public class Game {
     public void print() {
 
         Log.v("DREADER", "Date: " + datestring);
-
-
     }
+
 
     public String getDate() {
 
@@ -149,20 +206,24 @@ public class Game {
                 Util.DAY_OF_WEEK_NAMES[day], Util.SHORT_MONTH_NAMES[m], d);
     }
 
+
     public boolean isPending() {
 
         return status.equals("P");
     }
+
 
     public boolean isRegulation() {
 
         return status.equals("F");
     }
 
+
     public boolean isHome() {
 
         return location.equals("H");
     }
+
 
     public int getFinalScoreHome() {
 
@@ -171,10 +232,79 @@ public class Game {
         return Integer.parseInt(total);
     }
 
+
     public int getFinalScoreAway() {
 
         String total = goalsAway.get(goalsAway.size() - 1);
 
         return Integer.parseInt(total);
     }
+
+
+    @Override
+    public int describeContents() {
+
+        return 0;
+    }
+
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+
+        out.writeLong(nhl_id);
+        out.writeLong(datestring);
+
+        out.writeString(opponent);
+        out.writeString(location);
+
+        out.writeString(status);
+
+        if(isPending()) {
+
+            out.writeString(puckdrop);
+            out.writeString(networks);
+
+            return;
+        }
+
+        out.writeString(article);
+        out.writeString(tagline);
+
+        out.writeString(recapImage);
+        out.writeString(recapVideo);
+
+        for(String goal : goalsAway) {
+
+            out.writeString(goal);
+        }
+
+        for(String goal : goalsHome) {
+
+            out.writeString(goal);
+        }
+
+        for(String stat : statsAway) {
+
+            out.writeString(stat);
+        }
+
+        for(String stat : statsHome) {
+
+            out.writeString(stat);
+        }
+    }
+
+
+    public static final Creator<Game> CREATOR = new Creator<Game> () {
+
+        public Game createFromParcel(Parcel in) {
+
+            return new Game(in);
+        }
+
+        public Game[] newArray(int size) {
+
+            return new Game[size];
+        }
+    };
 }
