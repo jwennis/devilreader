4bad017b8bc9e7b445537df9d9e296769ca2c0dd
Joe <j.w.ennis@gmail.com>
2017-01-04 00:36:11 -0500

    feat: Bind Goal data to Views in GoalAdapter

--- app/src/main/java/com/example/dreader/devilreader/ui/GoalAdapter.java
+++ app/src/main/java/com/example/dreader/devilreader/ui/GoalAdapter.java
@@ -1,16 +1,22 @@
 package com.example.dreader.devilreader.ui;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.dreader.devilreader.R;
 import com.example.dreader.devilreader.model.Goal;
 
 import java.util.List;
 
+import butterknife.BindString;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class GoalAdapter extends RecyclerView.Adapter<GoalAdapter.GoalViewHolder> {
@@ -40,9 +46,16 @@ public class GoalAdapter extends RecyclerView.Adapter<GoalAdapter.GoalViewHolder
     public void onBindViewHolder(final GoalViewHolder holder, int position) {
 
         final Goal item = getItemAt(position);
+        final Context context = holder.layout_root.getContext();
 
+        Glide.with(context)
+                .load(holder.MUGSHOT_ROOT + item.getPlayer_id() + "@2x.jpg")
+                .transform(new CircleTransform(context))
+                .into(holder.mugshot);
 
-
+        holder.name.setText(String.format("%s (%d)", item.getName(), item.getCount()));
+        holder.assists.setText(item.getAssists());
+        holder.time.setText(item.getTime());
     }
 
 
@@ -63,6 +76,20 @@ public class GoalAdapter extends RecyclerView.Adapter<GoalAdapter.GoalViewHolder
 
         public ViewGroup layout_root;
 
+        @BindString(R.string.player_mugshot_root)
+        String MUGSHOT_ROOT;
+
+        @BindView(R.id.goal_mugshot)
+        ImageView mugshot;
+
+        @BindView(R.id.goal_name)
+        TextView name;
+
+        @BindView(R.id.goal_assists)
+        TextView assists;
+
+        @BindView(R.id.goal_time)
+        TextView time;
 
         public GoalViewHolder(View itemView) {
 
